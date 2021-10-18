82ef72f417f10d9e3b5a7339045daebf97d6715f
Eliana-Abate <eliana.abate@outlook.it>
2021-10-18 22:11:55 +0200

    added Carbon and formatted date

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -19,8 +19,8 @@
                     <tr>
                         <th scope="row">{{$post->id}}</th>
                         <td>{{$post->title}}</td>
-                        <td>{{$post->created_at}}</td>
-                        <td>{{$post->updated_at}}</td>
+                        <td>{{$post->getFormattedDate('created_at')}}</td>
+                        <td>{{$post->getFormattedDate('updated_at')}}</td>
                         <td>
                             <a href="{{route('admin.posts.show', $post->id)}}" class="btn btn-primary">Mostra</a>
                         </td>
