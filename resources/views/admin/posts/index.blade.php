958f9f229aa40e54bf6a43eb17563e2815a8fc47
Eliana-Abate <eliana.abate@outlook.it>
2021-10-18 21:36:02 +0200

    added table in posts\index

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layout.app')
+
+@section('content')
+<section>
+    <div class="container">
+        <h1>Elenco post:</h1>
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Created</th>
+                <th scope="col">Updated</th>
+                <th scope="col">Azioni</th>
+              </tr>
+            </thead>
+            <tbody>
+                @forelse ($posts as $post)
+                    <tr>
+                        <th scope="row">{{$post->id}}</th>
+                        <td>{{$post->title}}</td>
+                        <td>{{$post->created_at}}</td>
+                        <td>{{$post->updated_at}}</td>
+                        <td>
+                            <a href="{{route('admin.posts.show', $post->id)}}" class="btn btn-primary">Mostra</a>
+                        </td>
+                    </tr>
+                    
+                @empty
+                    <tr>
+                        <td colspan="5" class="text-center text-danger">Non sono presenti post</td>
+                    </tr>
+                    
+                @endforelse
+             
+            </tbody>
+          </table>
+
+    </div>
+</section>
+    
+@endsection
