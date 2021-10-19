1903525f017bfdacfbeda91eb56574291926e285
Eliana-Abate <eliana.abate@outlook.it>
2021-10-19 18:15:28 +0200

    added fillable

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    protected $fillable = ['title', 'content', 'image', 'slug'];
+
     public function getFormattedDate($column, $format = 'd-m-Y H:i:s') {
         return Carbon::create($this->$column)->format($format);
     }
