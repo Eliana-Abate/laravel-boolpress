3ee30a7b96b367c83d4651f344b1ba03c30ca9fa
Eliana-Abate <eliana.abate@outlook.it>
2021-10-19 15:53:07 +0200

    added use Faker\Database

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Database as Faker;
 
 class PostsTableSeeder extends Seeder
 {
@@ -11,6 +12,6 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        
     }
 }
