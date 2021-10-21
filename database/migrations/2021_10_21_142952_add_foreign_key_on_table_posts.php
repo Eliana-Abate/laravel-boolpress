de28de1a8a9194a5399f5b242037f5a2e68f40af
Eliana-Abate <eliana.abate@outlook.it>
2021-10-21 17:03:28 +0200

    added colum category_id and foreign key

--- database/migrations/2021_10_21_142952_add_foreign_key_on_table_posts.php
+++ database/migrations/2021_10_21_142952_add_foreign_key_on_table_posts.php
@@ -14,7 +14,11 @@ class AddForeignKeyOnTablePosts extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            //creo la colonna category_id
+            $table->unsignedBigInteger('category_id')->after('id')->nullable();
+
+            //definisco la foreign key - onDelete() mi risolve il problema di non cancellare i post di una categoria in caso di cancellazione della categoria stessa
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
         });
     }
 
@@ -26,7 +30,10 @@ class AddForeignKeyOnTablePosts extends Migration
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            //elimino prima la foreign key
+            $table->dropForeign('posts_category_id_foreign');
+            //dopo la colonna 
+            $table->dropColumn('category_id');
         });
     }
 }
