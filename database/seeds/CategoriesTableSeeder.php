10bcd7c5280b4bf1c8a3db4fef0a51362d9d5e4e
Eliana-Abate <eliana.abate@outlook.it>
2021-10-21 15:34:16 +0200

    added categories array and seeded DB

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+use Illuminate\Support\Str;
 
 class CategoriesTableSeeder extends Seeder
 {
@@ -11,6 +13,13 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $category_names = ['travel', 'food', 'lifestyle'];
+
+        foreach ($category_names as $name)
+            $category = new Category();
+            $category->name = $name;
+            $category->slug = Str::slug($name, '-');
+
+            $category->save();
     }
 }
