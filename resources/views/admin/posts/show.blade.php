ea2cbea13f5b89d6f2bdeedcc34fc1fad4ea8865
Eliana-Abate <eliana.abate@outlook.it>
2021-10-22 11:45:28 +0200

    added edit/delete buttons in posts\show.blade

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -11,9 +11,31 @@
                 <p>{{$post->content}}</p>
                 <address>Pubblicato il: {{$post->getFormattedDate('created_at')}}</address>
                 <address>Categoria: @if ($post->category) {{$post->category->name}} @else Nessuna categoria @endif</address>
-                <a href="{{route('admin.posts.index')}}" class="btn btn-primary">Indietro</a>
+                <div class="d-flex justify-content-end">
+                    <a href="{{route('admin.posts.index')}}" class="btn btn-primary mr-2">Indietro</a>
+                    <a class="btn btn-warning mr-2" href="{{route('admin.posts.edit', $post->id)}}">Modifica</a>
+                    <form method='POST' action="{{route('admin.posts.destroy', $post->id)}}" class="delete-post">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Cancella</button>
+                    </form>
+                </div> 
             </div>
         </div>
     </div>
 </section>
+@endsection
+
+@section('scripts')
+<script>
+    const deletePost = document.querySelectorAll('.delete-post'); 
+    deletePost.forEach(item =>{
+        item.addEventListener('submit', function (event) {
+            event.preventDefault();
+            const confirmEvent = confirm('Vuoi realmente eliminare questo Post?');
+            if (confirmEvent) this.submit();
+        })
+    })
+</script>
+    
 @endsection
