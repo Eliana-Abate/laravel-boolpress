c66eda660a3ea35da2ef8feb1605fac1a370c72d
Eliana-Abate <eliana.abate@outlook.it>
2021-10-28 19:58:11 +0200

    added attachment_cover in fillable

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'content', 'image', 'slug', 'category_id'];
+    protected $fillable = ['title', 'content', 'image', 'slug', 'category_id', 'attachment_cover'];
 
     public function getFormattedDate($column, $format = 'd-m-Y H:i:s') {
         return Carbon::create($this->$column)->format($format);
